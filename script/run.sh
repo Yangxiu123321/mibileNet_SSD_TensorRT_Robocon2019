f6f1f7c24d6b7622c26a209fa77eb3b00d0a5850
DOOM\Yang <13390560336@163.com>
2019-05-24 10:00:28 +0800

    修唉

--- script/run.sh
+++ script/run.sh
@@ -2,15 +2,4 @@
 
 cd ../build/build/bin
 
-./mobileNet
--m_red=/home/nvidia/code/tensorRT/mibileNet_SSD_TensorRT_Robocon2019/model/redblue/MobileNetSSD_deploy
--m_blue=/home/nvidia/code/tensorRT/mibileNet_SSD_TensorRT_Robocon2019/model/redblue/MobileNetSSD_deploy
--com=/dev/ttyS0 -m_alex=/home/nvidia/code/tensorRT/mibileNet_SSD_TensorRT_Robocon2019/model/alexnet/bvlc_alexnet
--m_mean=/home/nvidia/code/tensorRT/mibileNet_SSD_TensorRT_Robocon2019/model/alexnet/mean/imagenet_mean.binaryproto
--exposureTime=7670
--analogGain=2
--isUseAlex=false
--runTimeLimit=7
--boundaryErrorLimit=10
--isShowDebugImg=false
--boneConfidenceLimit=0.7
+./mobileNet -m_red=/home/nvidia/code/tensorRT/mibileNet_SSD_TensorRT_Robocon2019/model/redblue/MobileNetSSD_deploy -m_blue=/home/nvidia/code/tensorRT/mibileNet_SSD_TensorRT_Robocon2019/model/redblue/MobileNetSSD_deploy -com=/dev/ttyS0 -m_alex=/home/nvidia/code/tensorRT/mibileNet_SSD_TensorRT_Robocon2019/model/alexnet/bvlc_alexnet -m_mean=/home/nvidia/code/tensorRT/mibileNet_SSD_TensorRT_Robocon2019/model/alexnet/mean/imagenet_mean.binaryproto -exposureTime=7670 -analogGain=2 -isUseAlex=false -runTimeLimit=7 -boundaryErrorLimit=10 -isShowDebugImg=false -boneConfidenceLimit=0.7
